c5622bb4a78b5d5d62c6a3392a777465d43812b5
cvtkunalgharate <kunal.gharate@corevaluetech.com>
2022-03-30 22:02:31 +0530

    Updated Code

--- src/main/java/com/springsecdemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecdemo/security/ApplicationSecurityConfig.java
@@ -32,6 +32,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        //Test
         http
                 //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .csrf().disable()
